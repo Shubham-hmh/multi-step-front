da1b99283075e543e8c2a6838cadb38481f21efa
shubham-hmh <shubham_203054@saitm.org>
2024-06-20 14:47:54 +0530

    cors

--- index.js
+++ index.js
@@ -7,17 +7,12 @@ const { mongoose } = require('mongoose');
 
 
 app.use(express.json())
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: "http://localhost:3000",
-//   })
-// );
-
 app.use(
-  cors()
+  cors({
+    origin: 'https://multi-step-intigration.onrender.com', 
+    credentials: true,
+  })
 );
-
 mongoose.connect(process.env.MONGO_URL);
 
 app.get("/test", (req, res) => {
@@ -26,44 +21,44 @@ app.get("/test", (req, res) => {
 
 // Register Route
 app.post('/register', async (req, res) => {
-    const { username, email, password, firstName, lastName, address, phone, food} = req.body
-    try {
-        const userInfo = await User.create({
-            username,
-            email,
-            password,
-            firstName,
-            lastName,
-            address,
-            phone, 
-            food
-        })
-        res.json(userInfo)
-    } catch (error) {
-        res.status(422).json(error)
-    }
+  const { username, email, password, firstName, lastName, address, phone, food } = req.body
+  try {
+    const userInfo = await User.create({
+      username,
+      email,
+      password,
+      firstName,
+      lastName,
+      address,
+      phone,
+      food
+    })
+    res.json(userInfo)
+  } catch (error) {
+    res.status(422).json(error)
+  }
 })
 
 
 app.get('/users', async (req, res) => {
   try {
-      const users = await User.find();
-      res.json(users);
+    const users = await User.find();
+    res.json(users);
   } catch (error) {
-      res.status(500).json(error);
+    res.status(500).json(error);
   }
 });
 
 app.get('/users/:id', async (req, res) => {
   try {
-      const user = await User.findById(req.params.id);
-      if (user) {
-          res.json(user);
-      } else {
-          res.status(404).json({ error: 'User not found' });
-      }
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
   } catch (error) {
-      res.status(500).json(error);
+    res.status(500).json(error);
   }
 });
 
@@ -71,34 +66,34 @@ app.get('/users/:id', async (req, res) => {
 
 
 app.put('/users/:id', async (req, res) => {
-    const { data } = req.body;
-    try {
-      const user = await User.findByIdAndUpdate(req.params.id, data, { new: true });
-      if (user) {
-        res.json(user);
-      } else {
-        res.status(404).json({ error: 'User not found' });
-      }
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
-  
-
-app.delete('/users/:id', async (req, res) => {
+  const { data } = req.body;
   try {
-      const user = await User.findByIdAndDelete(req.params.id);
-      if (user) {
-          res.json({ message: 'User deleted successfully' });
-      } else {
-          res.status(404).json({ error: 'User not found' });
-      }
+    const user = await User.findByIdAndUpdate(req.params.id, data, { new: true });
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
   } catch (error) {
-      res.status(500).json(error);
+    res.status(500).json(error);
   }
 });
 
 
-app.listen(4000,()=>{
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (user) {
+      res.json({ message: 'User deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
+app.listen(4000, () => {
   console.log("server is running .......")
 });
